98fc2ae0c84af94253605fef2f4b5500738eeb2a
GRG02 <ratipong818@gmail.com>
2025-10-29 18:30:37 +0700

    add file visualization page

---
+++ app/visualization/page.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react";
+import { supabase } from "../lib/supabaseClient"; // KLA : import supabase client
+import { useRouter } from "next/navigation"; // KLA : import useRouter ด้านบน
+import axios from "axios";
+
+const VisualizationPage = () => {
+    return(
+        <div>
+            VisualizationPage
+        </div>
+    );
+}
+
+export default VisualizationPage;
