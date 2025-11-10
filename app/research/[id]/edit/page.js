30a83f954280ee546c31d5d2cc73a7636219f734
Intorn Rungrodatsawakul <s6552410027@sau.ac.th>
2025-11-10 11:09:40 +0700

    Fix error vercel

--- app/research/[id]/edit/page.js
+++ app/research/[id]/edit/page.js
@@ -2,7 +2,7 @@
 
 import React, { useEffect, useState } from "react";
 import { useParams, useRouter } from "next/navigation";
-import { supabase } from "../../lib/supabaseClient";
+import { supabase } from "../../../lib/supabaseClient";
 
 const STORAGE_BUCKET = "user_bk";
 
